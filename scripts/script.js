25e85bdf3ba08f9108df8d4ec7295c5a07704d2f
Mennessier Loïc <152868429+lmennessier@users.noreply.github.com>
2024-10-26 23:12:01 +0200

    Delete scripts directory

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('fa-xmark');  // Change l'icÃ´ne
-    navbar.classList.toggle('active');  // Active le menu
-}
